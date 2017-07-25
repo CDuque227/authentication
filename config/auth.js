a97c5d1927e39b3c86815b1113614bbbf43d600d
CDuque227 <carlos.duque@payco.co>
2017-07-25 12:57:45 -0500

    authentication

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID'        : '258950334600241',
+        'clientSecret'    : 'd89399b739cb9fb2e90053124bce565a',
+        'callbackURL'     : 'http://localhost:3000/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.10/me?fields=first_name,last_name,email'
+    },
+};
---
+++ config/passport.js
@@ -0,0 +1,173 @@
+var LocalStrategy    = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var User = require('../app/models/user');
+var configAuth = require('./auth');
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    // LOCAL LOGIN
+    passport.use('local-login', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true
+    },
+    function(req, email, password, done) {
+
+        if (email)
+            email = email.toLowerCase();
+
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+                else
+                    return done(null, user);
+            });
+        });
+
+    }));
+
+    // LOCAL SIGNUP
+    passport.use('local-signup', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true
+    },
+    function(req, email, password, done) {
+        if (email)
+            email = email.toLowerCase();
+
+        // asynchronous
+        process.nextTick(function() {
+            if (!req.user) {
+                User.findOne({ 'local.email' :  email }, function(err, user) {
+                    if (err)
+                        return done(err);
+
+                    if (user) {
+                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                    } else {
+
+                        var newUser = new User();
+
+                        newUser.local.email = email;
+                        newUser.local.password = newUser.generateHash(password);
+
+                        newUser.save(function(err) {
+                            if (err)
+                                return done(err);
+
+                            return done(null, newUser);
+                        });
+                    }
+
+                });
+            } else if ( !req.user.local.email ) {
+                User.findOne({ 'local.email' :  email }, function(err, user) {
+                    if (err)
+                        return done(err);
+
+                    if (user) {
+                        return done(null, false, req.flash('loginMessage', 'That email is already taken.'));
+                    } else {
+                        var user = req.user;
+                        user.local.email = email;
+                        user.local.password = user.generateHash(password);
+                        user.save(function (err) {
+                            if (err)
+                                return done(err);
+
+                            return done(null,user);
+                        });
+                    }
+                });
+            } else {
+                return done(null, req.user);
+            }
+
+        });
+
+    }));
+
+    // FACEBOOK
+    var fbStrategy = configAuth.facebookAuth;
+    fbStrategy.passReqToCallback = true;
+    passport.use(new FacebookStrategy(fbStrategy,
+    function(req, token, refreshToken, profile, done) {
+        process.nextTick(function() {
+
+            if (!req.user) {
+
+                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                    if (err)
+                        return done(err);
+
+                    if (user) {
+
+                        if (!user.facebook.token) {
+                            user.facebook.token = token;
+                            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                            user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                            user.save(function(err) {
+                                if (err)
+                                    return done(err);
+
+                                return done(null, user);
+                            });
+                        }
+
+                        return done(null, user);
+                    } else {
+                        var newUser = new User();
+
+                        newUser.facebook.id = profile.id;
+                        newUser.facebook.token = token;
+                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                        newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                        newUser.save(function(err) {
+                            if (err)
+                                return done(err);
+
+                            return done(null, newUser);
+                        });
+                    }
+                });
+
+            } else {
+                var user = req.user;
+
+                user.facebook.id = profile.id;
+                user.facebook.token = token;
+                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                user.save(function(err) {
+                    if (err)
+                        return done(err);
+
+                    return done(null, user);
+                });
+
+            }
+        });
+
+    }));
+};
